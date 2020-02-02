ab139f812b3f7b0748a2799f3fb393a6d7e15524
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-02-02 16:32:21 -0300

    fix eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
   rules: {
     "prettier/prettier": "error",
     "class-methods-use-this": "off",
-    "no-param-reassingn": "off",
+    "no-param-reassign": "off",
     "camelcase": "off",
     "no-unused-vars": ["error", {"argsIgnorePattern": "next"}]
   },
