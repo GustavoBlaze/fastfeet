5ab3940c9d2535ed1700f91408546ef1ad82da9f
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-02-09 16:25:32 -0300

    auth middleware send http code for invalid token

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -17,6 +17,6 @@ export default async (req, res, next) => {
 
     return next();
   } catch (err) {
-    return res.json(401).json({ error: 'Token invalid' });
+    return res.status(401).json({ error: 'Token invalid' });
   }
 };
