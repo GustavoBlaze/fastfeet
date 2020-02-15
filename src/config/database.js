fcdfd3dfdaa01659c09e32c57cf9eb10ec9eea67
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-02-15 13:54:46 -0300

    refator: change env variable name

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ module.exports = {
   dialect: 'postgres',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   define: {
