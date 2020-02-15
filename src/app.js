9aa9650d54334384b445fabc6e7b18df69432223
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-02-15 18:44:13 -0300

    feat: add sentry to parse and store errors

--- src/app.js
+++ src/app.js
@@ -1,18 +1,27 @@
 import 'dotenv/config';
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry';
+
 import './database';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -22,6 +31,18 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+      return res.status(500).json({ error: 'Internal Error' });
+    });
   }
 }
 
