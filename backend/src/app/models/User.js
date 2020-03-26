cba844f8b700de034033d705f4fd4b6ec2e91829
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-03-25 21:12:56 -0300

    fix: add password field to user model

--- backend/src/app/models/User.js
+++ backend/src/app/models/User.js
@@ -7,6 +7,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
       {
