25c3db62c2141897ea557b09c0adfc9fd1690f41
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-03-27 20:29:24 -0300

    style: add scroll on page content

--- frontend/src/pages/_layouts/default/styles.js
+++ frontend/src/pages/_layouts/default/styles.js
@@ -4,6 +4,7 @@ export const Wrapper = styled.div`
   background: #f5f5f5;
   height: 100%;
   flex-direction: column;
+  overflow: auto;
 `;
 
 export const Content = styled.div`
@@ -11,4 +12,5 @@ export const Content = styled.div`
   margin-bottom: 0;
   width: 100%;
   max-width: calc(1440px - 120px * 2);
+  flex: 1;
 `;
