b067eacca24e7fc15b49a85ae3d5d4bae9bf5aee
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-03-28 19:28:15 -0300

    feat: add logout function on header

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { Container } from './styles';
 import logo from '~/assets/logo.png';
 
+import * as AuthActions from '~/store/modules/auth/actions';
+
 export default function Header({ locationName = undefined }) {
+  const dispatch = useDispatch();
+
+  function handleSignOut() {
+    dispatch(AuthActions.signOut());
+  }
+
   return (
     <Container locationName={locationName}>
       <Link to="/deliveries">
@@ -27,7 +36,9 @@ export default function Header({ locationName = undefined }) {
 
       <div>
         <strong>Admin FastFeet</strong>
-        <button type="button">Sair do sistema</button>
+        <button type="button" onClick={handleSignOut}>
+          Sair do sistema
+        </button>
       </div>
     </Container>
   );
