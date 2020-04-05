2f47e1fcb0d703131d9189a0a96a302973e37390
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-04-05 17:10:24 -0300

    feat: add pagination on deliveriesstatus

--- backend/src/app/controllers/DeliveryStatusController.js
+++ backend/src/app/controllers/DeliveryStatusController.js
@@ -5,12 +5,15 @@ import Delivery from '../models/Delivery';
 class DeliveryStatusController {
   async index(req, res) {
     const completed = req.query.completed || false;
+    const { page = 1 } = req.query;
     const deliveries = await Delivery.findAll({
       where: {
         deliveryman_id: req.params.id,
         canceled_at: null,
         signature_id: completed ? { [Op.ne]: null } : null,
       },
+      limit: 6,
+      offset: (page - 1) * 6,
       order: ['createdAt'],
       attributes: ['id', 'product', 'start_date', 'end_date', 'createdAt'],
       include: [
