5c4da3c85226b44c2a0d7be935b7be34567b1f68
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-04-05 17:59:31 -0300

    feat: create profile page

--- mobile/src/pages/Profile/index.js
+++ mobile/src/pages/Profile/index.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { format, parseISO } from 'date-fns';
+import PropTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
+import {
+  Container,
+  AvatarContainer,
+  Avatar,
+  Title,
+  SubTitle,
+  LogoutButton,
+} from './styles';
 
 export default function Profile() {
-  return <Container />;
+  const profile = useSelector((store) => store.deliveryman.profile);
+  const formattedData = format(parseISO(profile.createdAt), 'dd/MM/yyyy');
+  const dispatch = useDispatch();
+
+  function handleLogout() {
+    dispatch(signOut());
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="#FFF" />
+      <Container>
+        <AvatarContainer>
+          <Avatar name={profile.name} avatar={profile.avatar} />
+        </AvatarContainer>
+
+        <Title>Nome Completo</Title>
+        <SubTitle>{profile.name}</SubTitle>
+
+        <Title>Email</Title>
+        <SubTitle>{profile.email}</SubTitle>
+
+        <Title>Data de cadastro</Title>
+        <SubTitle>{formattedData}</SubTitle>
+
+        <LogoutButton onPress={handleLogout}>Logout</LogoutButton>
+      </Container>
+    </>
+  );
 }
+
+const TabBarIcon = ({ tintColor }) => (
+  <Icon name="account-circle" size={20} color={tintColor} />
+);
+
+TabBarIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+Profile.navigationOptions = {
+  tabBarLabel: 'Entregas',
+  tabBarIcon: TabBarIcon,
+};
--- mobile/src/pages/Profile/styles.js
+++ mobile/src/pages/Profile/styles.js
@@ -1,3 +1,45 @@
 import styled from 'styled-components/native';
+import LetterAvatar from '~/components/Avatar';
+import Button from '~/components/Button';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  flex-direction: column;
+  padding: 50px 35px;
+  align-items: center;
+`;
+
+export const AvatarContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+  margin-bottom: 25px;
+`;
+
+export const Avatar = styled(LetterAvatar).attrs({
+  size: 136,
+})``;
+
+export const Title = styled.Text`
+  color: #666666;
+  font-size: 12px;
+  align-self: stretch;
+  text-align: left;
+  margin-top: 15px;
+`;
+
+export const SubTitle = styled.Text`
+  align-self: stretch;
+  text-align: left;
+  color: #444444;
+  font-size: 22px;
+  font-weight: bold;
+`;
+
+export const LogoutButton = styled(Button).attrs({
+  color: '#E74040',
+})`
+  align-self: stretch;
+  margin-top: 30px;
+`;
