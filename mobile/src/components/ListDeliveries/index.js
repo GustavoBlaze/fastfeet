6e6360600d44be29755e3e105acf30682823779f
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-04-05 17:13:37 -0300

    feat: loading more, push to refresh and animation

--- mobile/src/components/ListDeliveries/index.js
+++ mobile/src/components/ListDeliveries/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { format, parseISO } from 'date-fns';
 import { Alert } from 'react-native';
 import { useSelector } from 'react-redux';
@@ -12,7 +12,15 @@ import {
   FilterText,
   List,
   Loading,
+  Empty,
+  EmptyLabel,
+  Lottie,
+  LoadingMoreContainer,
+  LoadingMoreSpinner,
+  LoadingMoreText,
 } from './styles';
+
+import noVisibility from '~/assets/visibility-off.json';
 
 import api from '~/services/api';
 
@@ -22,7 +30,10 @@ export default function ListDeliveries() {
   const [filter, setFilter] = useState('pending');
   const [deliveries, setDeliveries] = useState([]);
   const [loading, setLoading] = useState(false);
-
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
   const id = useSelector((store) => store.deliveryman.profile.id);
 
   function parseDeliveries(items) {
@@ -33,27 +44,81 @@ export default function ListDeliveries() {
     }));
   }
 
-  useEffect(() => {
-    async function loadDeliveries() {
-      setLoading(true);
-      try {
-        let url = `deliveryman/${id}/deliveries`;
-        if (filter === 'delivered') url += '?completed=true';
+  const loadDeliveries = useCallback(async () => {
+    setDeliveries([]);
+    setLoading(true);
+    try {
+      let url = `deliveryman/${id}/deliveries`;
+      if (filter === 'delivered') url += '?completed=true';
 
-        const { data } = await api.get(url);
-        setDeliveries(parseDeliveries(data));
-      } catch (err) {
-        Alert.alert(
-          'Falha na requisição',
-          'Não foi possível buscar as entregas, por favor tente mais tarde.'
-        );
+      const { data } = await api.get(url);
+      setDeliveries(parseDeliveries(data));
+    } catch (err) {
+      Alert.alert(
+        'Falha na requisição',
+        'Não foi possível buscar as entregas, por favor tente mais tarde.'
+      );
+    }
+    setLoading(false);
+  }, [id, filter]);
+
+  useEffect(() => {
+    loadDeliveries();
+  }, [loadDeliveries]);
+
+  const handleRefresh = useCallback(async () => {
+    setHasMore(false);
+    setRefreshing(true);
+    setDeliveries([]);
+    setPage(1);
+
+    try {
+      let url = `deliveryman/${id}/deliveries`;
+      if (filter === 'delivered') url += '?completed=true';
+
+      const { data } = await api.get(url);
+      setDeliveries(parseDeliveries(data));
+    } catch (err) {
+      Alert.alert(
+        'Falha na requisição',
+        'Não foi possível buscar as entregas, por favor tente mais tarde.'
+      );
+    }
+    setRefreshing(false);
+    setHasMore(true);
+  }, [id, filter]);
+
+  const loadMore = useCallback(async () => {
+    if (loadingMore || !hasMore) return;
+
+    setLoadingMore(true);
+
+    try {
+      const params = { page: page + 1 };
+      const url = `deliveryman/${id}/deliveries`;
+
+      if (filter === 'delivered') {
+        params.delivered = true;
       }
-      setLoading(false);
+
+      const { data } = await api.get(url, { params });
+
+      if (data.length > 0) {
+        setDeliveries([...deliveries, ...parseDeliveries(data)]);
+        setPage(page + 1);
+        console.tron.log(page);
+      } else {
+        setHasMore(false);
+      }
+    } catch (err) {
+      Alert.alert(
+        'Falha na requisição',
+        'Não foi possível buscar as entregas, por favor tente mais tarde.'
+      );
     }
 
-    setDeliveries([]);
-    loadDeliveries();
-  }, [id, filter]);
+    setLoadingMore(false);
+  }, [hasMore, loadingMore, id, filter, page, deliveries]);
 
   return (
     <Container>
@@ -71,11 +136,34 @@ export default function ListDeliveries() {
       {loading ? (
         <Loading />
       ) : (
-        <List
-          data={deliveries}
-          keyExtractor={(delivery) => String(delivery.id)}
-          renderItem={({ item }) => <DeliveryCard delivery={item} />}
-        />
+        <>
+          {deliveries.length > 0 || refreshing ? (
+            <>
+              <List
+                data={deliveries}
+                keyExtractor={(delivery) => String(delivery.id)}
+                renderItem={({ item }) => <DeliveryCard delivery={item} />}
+                refreshing={refreshing}
+                onRefresh={handleRefresh}
+                onEndReachedThreshold={0.5}
+                onEndReached={loadMore}
+                ListFooterComponent={
+                  hasMore && (
+                    <LoadingMoreContainer>
+                      <LoadingMoreSpinner />
+                      <LoadingMoreText>Carregando...</LoadingMoreText>
+                    </LoadingMoreContainer>
+                  )
+                }
+              />
+            </>
+          ) : (
+            <Empty>
+              <Lottie source={noVisibility} autoPlay loop />
+              <EmptyLabel>Sem entregas por aqui</EmptyLabel>
+            </Empty>
+          )}
+        </>
       )}
     </Container>
   );
--- mobile/src/components/ListDeliveries/styles.js
+++ mobile/src/components/ListDeliveries/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
 import { ActivityIndicator } from 'react-native';
+import LottieView from 'lottie-react-native';
 
 export const Container = styled.View`
   flex: 1;
@@ -43,10 +44,46 @@ export const Loading = styled(ActivityIndicator).attrs({
   align-items: center;
 `;
 
+export const LoadingMoreContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 0px 5px;
+  padding-bottom: 10px;
+`;
+
+export const LoadingMoreSpinner = styled(ActivityIndicator).attrs({
+  size: 'small',
+  color: '#7D40E7',
+})``;
+
+export const LoadingMoreText = styled.Text`
+  color: #999;
+  font-size: 14px;
+  font-weight: bold;
+  margin-left: 10px;
+`;
 export const List = styled.FlatList.attrs({
   showsVerticalScrollIndicator: false,
 })`
   margin-top: 20px;
   margin-left: -3px;
   margin-right: -3px;
+`;
+
+export const Empty = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Lottie = styled(LottieView)`
+  width: 30px;
+  height: 30px;
+`;
+
+export const EmptyLabel = styled.Text`
+  color: #ddd;
+  font-size: 14px;
+  font-weight: bold;
+  margin-top: 5px;
 `;
